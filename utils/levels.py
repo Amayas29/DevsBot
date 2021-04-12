cd347aa5be9f911bc79dfa42ec73b2cb8d84ab5c
Amayas29 <amayas29sd@gmail.com>
2021-04-12 17:10:47 +0200

    Must change : now database is user

--- utils/levels.py
+++ utils/levels.py
@@ -14,203 +14,213 @@ USERS_PATH = f"{root_dir}/resources/users.json"
 
 
 def update_users(remove, *users):
-    try:
+    # try:
 
-        global all_users
+    #     global all_users
 
-        with open(USERS_PATH) as data:
-            users_dict = json.load(data)
+    #     with open(USERS_PATH) as data:
+    #         users_dict = json.load(data)
 
-        for user in users:
-            if remove:
-                users_dict.pop(str(user.id))
+    #     for user in users:
+    #         if remove:
+    #             users_dict.pop(str(user.id))
 
-            elif str(user.id) not in users_dict:
+    #         elif str(user.id) not in users_dict:
 
-                users_dict[str(user.id)] = {
-                    "level": 1,
-                    "exp": 0,
-                    "warns": 0,
-                    "birth_date": "NaN",
-                    "old_message": None
-                }
+    #             users_dict[str(user.id)] = {
+    #                 "level": 1,
+    #                 "exp": 0,
+    #                 "warns": 0,
+    #                 "birth_date": "NaN",
+    #                 "old_message": None
+    #             }
 
-        all_users = users_dict
+    #     all_users = users_dict
 
-        with open(USERS_PATH, "w") as file:
-            json.dump(users_dict, file, indent=4)
+    #     with open(USERS_PATH, "w") as file:
+    #         json.dump(users_dict, file, indent=4)
 
-    except:
-        traceback.print_exc()
+    # except:
+    #     traceback.print_exc()
+
+    pass
 
 
 def set_exp(user, exp):
 
-    # todo :
-    settings = None
-    for role in user.roles:
-        if role.id in settings.ignored_roles_levels or role.is_integration(
-        ) or role.is_bot_managed():
-            return
+    # settings = None
+    # for role in user.roles:
+    #     if role.id in settings.ignored_roles_levels or role.is_integration(
+    #     ) or role.is_bot_managed():
+    #         return
 
-    global all_users
+    # global all_users
 
-    try:
-        if all_users == {} or str(user.id) not in all_users:
-            with open(USERS_PATH) as data:
-                all_users = json.load(data)
+    # try:
+    #     if all_users == {} or str(user.id) not in all_users:
+    #         with open(USERS_PATH) as data:
+    #             all_users = json.load(data)
 
-        if str(user.id) not in all_users:
-            now = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-            all_users[str(user.id)] = {
-                "level": 1,
-                "exp": exp,
-                "warns": 0,
-                "birth_date": "NaN",
-                "old_message": now
-            }
+    #     if str(user.id) not in all_users:
+    #         now = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    #         all_users[str(user.id)] = {
+    #             "level": 1,
+    #             "exp": exp,
+    #             "warns": 0,
+    #             "birth_date": "NaN",
+    #             "old_message": now
+    #         }
 
-        else:
-            old_message = all_users[str(user.id)]["old_message"]
-            now = datetime.now()
+    #     else:
+    #         old_message = all_users[str(user.id)]["old_message"]
+    #         now = datetime.now()
 
-            if old_message == None:
-                old_message = now
-            else:
-                old_message = datetime.strptime(old_message,
-                                                "%d-%m-%Y %H:%M:%S")
+    #         if old_message == None:
+    #             old_message = now
+    #         else:
+    #             old_message = datetime.strptime(old_message,
+    #                                             "%d-%m-%Y %H:%M:%S")
 
-            diff = floor(((now - old_message).total_seconds() / 60))
+    #         diff = floor(((now - old_message).total_seconds() / 60))
 
-            if diff >= settings.min_time:
-                all_users[str(user.id)]["exp"] += exp
+    #         if diff >= settings.min_time:
+    #             all_users[str(user.id)]["exp"] += exp
 
-            all_users[str(
-                user.id)]["old_message"] = now.strftime("%d-%m-%Y %H:%M:%S")
+    #         all_users[str(
+    #             user.id)]["old_message"] = now.strftime("%d-%m-%Y %H:%M:%S")
 
-        with open(USERS_PATH, "w") as file:
-            json.dump(all_users, file, indent=4)
+    #     with open(USERS_PATH, "w") as file:
+    #         json.dump(all_users, file, indent=4)
 
-    except:
-        traceback.print_exc()
+    # except:
+    #     traceback.print_exc()
+
+    pass
 
 
 def level_up(user):
 
-    # todo :
-    settings = None
-    for role in user.roles:
-        if role.id in settings.ignored_roles_levels or role.is_integration(
-        ) or role.is_bot_managed():
-            return False, -1
+    # settings = None
+    # for role in user.roles:
+    #     if role.id in settings.ignored_roles_levels or role.is_integration(
+    #     ) or role.is_bot_managed():
+    #         return False, -1
 
-    global all_users
+    # global all_users
 
-    try:
-        if all_users == {} or str(user.id) not in all_users:
-            with open(USERS_PATH) as data:
-                all_users = json.load(data)
+    # try:
+    #     if all_users == {} or str(user.id) not in all_users:
+    #         with open(USERS_PATH) as data:
+    #             all_users = json.load(data)
 
-        if str(user.id) not in all_users:
-            return False
+    #     if str(user.id) not in all_users:
+    #         return False
 
-        id = str(user.id)
-        user = all_users[id]
+    #     id = str(user.id)
+    #     user = all_users[id]
 
-        level = user["level"]
-        exp = user["exp"]
+    #     level = user["level"]
+    #     exp = user["exp"]
 
-        max_exp = 50 * level**2 - 50 * level + 200
+    #     max_exp = 50 * level**2 - 50 * level + 200
 
-        if exp > max_exp:
-            user["level"] += 1
-            user["exp"] = 0
-            all_users[id] = user
+    #     if exp > max_exp:
+    #         user["level"] += 1
+    #         user["exp"] = 0
+    #         all_users[id] = user
 
-            # TODO set level role ...
-            with open(USERS_PATH, "w") as file:
-                json.dump(all_users, file, indent=4)
+    #         # TODO set level role ...
+    #         with open(USERS_PATH, "w") as file:
+    #             json.dump(all_users, file, indent=4)
 
-            return True, user["level"]
+    #         return True, user["level"]
 
-        return False, user["level"]
+    #     return False, user["level"]
 
-    except:
-        traceback.print_exc()
-        return False, -1
+    # except:
+    #     traceback.print_exc()
+    #     return False, -1
+
+    pass
 
 
 def get_top_users():
 
-    global all_users
+    # global all_users
 
-    try:
+    # try:
 
-        with open(USERS_PATH) as data:
-            all_users = json.load(data)
+    #     with open(USERS_PATH) as data:
+    #         all_users = json.load(data)
 
-        tops = {}
-        for user, values in all_users.items():
+    #     tops = {}
+    #     for user, values in all_users.items():
 
-            if values["level"] == None or values["exp"] == None:
-                continue
+    #         if values["level"] == None or values["exp"] == None:
+    #             continue
 
-            tops[user] = [values["level"], values["exp"]]
+    #         tops[user] = [values["level"], values["exp"]]
 
-        tops = list(
-            sorted(tops.items(), key=lambda item: item[1], reverse=True))
-        return tops
+    #     tops = list(
+    #         sorted(tops.items(), key=lambda item: item[1], reverse=True))
+    #     return tops
 
-    except:
-        traceback.print_exc()
-        return []
+    # except:
+    #     traceback.print_exc()
+    #     return []
+
+    pass
 
 
 def get_price(user):
 
-    global all_users
+    # global all_users
 
-    try:
-        if all_users == {} or str(user.id) not in all_users:
-            with open(USERS_PATH) as data:
-                all_users = json.load(data)
+    # try:
+    #     if all_users == {} or str(user.id) not in all_users:
+    #         with open(USERS_PATH) as data:
+    #             all_users = json.load(data)
 
-        if str(user.id) not in all_users:
-            return 0
+    #     if str(user.id) not in all_users:
+    #         return 0
 
-        level = all_users[str(user.id)]["level"]
+    #     level = all_users[str(user.id)]["level"]
 
-        if level == None:
-            return 5564800000
+    #     if level == None:
+    #         return 5564800000
 
-        return 50000 * level
-    except:
-        traceback.print_exc()
-        return 0
+    #     return 50000 * level
+    # except:
+    #     traceback.print_exc()
+    #     return 0
+
+    pass
 
 
 def get_users_birthday():
 
-    global all_users
+    # global all_users
 
-    liste = []
-    try:
-        with open(USERS_PATH) as data:
-            all_users = json.load(data)
+    # liste = []
+    # try:
+    #     with open(USERS_PATH) as data:
+    #         all_users = json.load(data)
 
-        now = datetime.strftime(datetime.now(), "%d-%m-%Y")
-        now = datetime.strptime(now, "%d-%m-%Y")
-        for user, values in all_users.items():
-            try:
-                birthdate = datetime.strptime(values["birth_date"], "%d-%m-%Y")
+    #     now = datetime.strftime(datetime.now(), "%d-%m-%Y")
+    #     now = datetime.strptime(now, "%d-%m-%Y")
+    #     for user, values in all_users.items():
+    #         try:
+    #             birthdate = datetime.strptime(values["birth_date"], "%d-%m-%Y")
 
-                if birthdate.month == now.month and birthdate.day == now.day:
-                    age = floor((now - birthdate).total_seconds() / 31536000)
-                    liste.append((user, age))
-            except:
-                continue
+    #             if birthdate.month == now.month and birthdate.day == now.day:
+    #                 age = floor((now - birthdate).total_seconds() / 31536000)
+    #                 liste.append((user, age))
+    #         except:
+    #             continue
 
-        return liste
-    except:
-        traceback.print_exc()
-        return []
+    #     return liste
+    # except:
+    #     traceback.print_exc()
+    #     return []
+
+    pass

4c49a675e09c3ec3f7aaa24f2bec844e1fa90259
Amayas29 <amayas29sd@gmail.com>
2021-04-12 17:11:39 +0200

    Must change now the bot is a multiguild

--- utils/rolemenu.py
+++ utils/rolemenu.py
@@ -5,38 +5,40 @@ import traceback
 
 async def add_reaction_verification(bot, payload):
 
-    # todo :
-    settings = None
-    guild = bot.get_guild(payload.guild_id)
-    try:
-        if payload.message_id == settings.verification_message and payload.emoji.name == settings.verification_emoji:
-            verified_role = guild.get_role(settings.verified_role)
-            if verified_role in payload.member.roles:
-                return
+    # settings = None
+    # guild = bot.get_guild(payload.guild_id)
+    # try:
+    #     if payload.message_id == settings.verification_message and payload.emoji.name == settings.verification_emoji:
+    #         verified_role = guild.get_role(settings.verified_role)
+    #         if verified_role in payload.member.roles:
+    #             return
 
-        for role in payload.member.roles:
-            if role.is_integration() or role.is_bot_managed():
-                return
+    #     for role in payload.member.roles:
+    #         if role.is_integration() or role.is_bot_managed():
+    #             return
 
-        for role in settings.initial_roles:
-            role_discord = guild.get_role(role)
-            await payload.member.add_roles(role_discord)
+    #     for role in settings.initial_roles:
+    #         role_discord = guild.get_role(role)
+    #         await payload.member.add_roles(role_discord)
 
-    except:
-        traceback.print_exc()
+    # except:
+    #     traceback.print_exc()
+
+    pass
 
 
 async def delete_reactions(member):
 
-    try:
-        # todo :
-        settings = None
-        server = member.guild
-        verification = settings.channels["verification"]
-        verification = server.get_channel(verification)
-        verification_message = await verification.fetch_message(
-            settings.verification_message)
-        await verification_message.remove_reaction(settings.verification_emoji,
-                                                   member)
-    except:
-        traceback.print_exc()
+    # try:
+    #     settings = None
+    #     server = member.guild
+    #     verification = settings.channels["verification"]
+    #     verification = server.get_channel(verification)
+    #     verification_message = await verification.fetch_message(
+    #         settings.verification_message)
+    #     await verification_message.remove_reaction(settings.verification_emoji,
+    #                                                member)
+    # except:
+    #     traceback.print_exc()
+
+    pass
